de267bf4021284c2f09c0b94a94bddeb18568af5
king chen <396885563@qq.com>
2018-03-02 16:21:18 +0800

    实现dubbo接口泛型暴露、引用 GenericService。同时使用多协议（dubbo、hessian）/hessian用于大数据传输

---
+++ common/src/main/java/com/king/common/utils/JsonResponse.java
@@ -0,0 +1,55 @@
+package com.king.common.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 返回数据--默认code：200 成功；500失败;
+ * @author King chen
+ * @date 2017年12月25日
+ */
+public class JsonResponse extends HashMap<String, Object> {
+	private static final long serialVersionUID = 1L;
+	
+	public JsonResponse() {
+		put("code", 200);
+	}
+	
+	public static JsonResponse error() {
+		return error(500, "未知异常，请联系管理员");
+	}
+	
+	public static JsonResponse error(String msg) {
+		return error(500, msg);
+	}
+	
+	public static JsonResponse error(int code, String msg) {
+		JsonResponse r = new JsonResponse();
+		r.put("code", code);
+		r.put("msg", msg);
+		return r;
+	}
+
+	public static JsonResponse success(String msg) {
+		JsonResponse r = new JsonResponse();
+		r.put("msg", msg);
+		return r;
+	}
+	
+	public static JsonResponse success(Map<String, Object> map) {
+		JsonResponse r = new JsonResponse();
+		r.putAll(map);
+		return r;
+	}
+	
+	public static JsonResponse success() {
+		JsonResponse r = new JsonResponse();
+		r.put("msg", "success");
+		return r;
+	}
+
+	public JsonResponse put(String key, Object value) {
+		super.put(key, value);
+		return this;
+	}
+}
--- common/src/main/java/com/king/common/utils/SpringContextUtils.java
+++ common/src/main/java/com/king/common/utils/SpringContextUtils.java
@@ -6,6 +6,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+import com.sun.org.apache.regexp.internal.recompile;
+
 /**
  * Spring Context 工具类
  * @author King chen
@@ -26,6 +28,10 @@ public class SpringContextUtils implements ApplicationContextAware {
 		return applicationContext.getBean(name);
 	}
 
+	public static <T> T getBean(Class<T> bean){
+		return applicationContext.getBean(bean);
+	}
+	
 	public static <T> T getBean(String name, Class<T> requiredType) {
 		return applicationContext.getBean(name, requiredType);
 	}
