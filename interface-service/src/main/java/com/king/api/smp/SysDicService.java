1288adf0c7a89bcd53018916492a48619a8d504d
king chen <396885563@qq.com>
2018-05-11 18:04:04 +0800

    增加数据字典、代码优化

---
+++ interface-service/src/main/java/com/king/api/smp/SysDicService.java
@@ -0,0 +1,31 @@
+package com.king.api.smp;
+
+
+import java.util.List;
+
+import com.king.dal.gen.model.smp.SysDic;
+import com.king.dal.gen.service.BaseService;
+
+/**
+ * 数据字典明细表
+ * 
+ * @author king chen
+ * @email 396885563@qq.com
+ * @date 2018-05-08 17:26:32
+ */
+public interface SysDicService extends BaseService<SysDic>{
+	
+	/**
+	 * 根据字典编码查询字典项
+	 * @param code
+	 * @return
+	 */
+	List<SysDic> queryDicList(String code);
+	
+	/**
+	 * 根据父节点查询
+	 * @param parentd
+	 * @return
+	 */
+	List<SysDic> queryParentList(long parentId);
+}
