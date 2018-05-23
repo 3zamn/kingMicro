d4d3581027c0866961afaadfe092517cff9ecd57
king chen <396885563@qq.com>
2018-05-23 15:01:57 +0800

    云盘

---
+++ interface-service/src/main/java/com/king/api/oss/SysOssService.java
@@ -0,0 +1,17 @@
+package com.king.api.oss;
+
+import com.king.dal.gen.model.oss.SysOss;
+import com.king.dal.gen.service.BaseService;
+
+
+/**
+ * 文件上传
+ * 
+ * @author king chen
+ * @email 396885563@qq.com
+ * @date 2018-05-23 14:25:16
+ */
+public interface SysOssService extends BaseService<SysOss>{
+	
+	
+}
