a413248cd73876f0bf24830536d7bba1daae9aab
king chen <396885563@qq.com>
2018-05-23 15:01:28 +0800

    云盘

---
+++ common/src/main/java/com/king/dal/gen/model/oss/SysOss.java
@@ -0,0 +1,97 @@
+package com.king.dal.gen.model.oss;
+
+import java.io.Serializable;
+import java.util.Date;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+
+
+/**
+ * 文件上传
+ * 
+ * @author king chen
+ * @email 396885563@qq.com
+ * @date 2018-05-23 14:25:16
+ */
+ @ApiModel("文件上传")
+public class SysOss implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	//
+	@ApiModelProperty("")
+	private Long id;
+	//类型
+	@ApiModelProperty("类型")
+	private String type;
+	//URL地址
+	@ApiModelProperty("URL地址")
+	private String url;
+	//创建者
+	@ApiModelProperty("创建者")
+	private String creator;
+	//创建时间
+	@ApiModelProperty("创建时间")
+	private Date createDate;
+
+	/**
+	 * 设置：
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * 获取：
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * 设置：类型
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	/**
+	 * 获取：类型
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * 设置：URL地址
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	/**
+	 * 获取：URL地址
+	 */
+	public String getUrl() {
+		return url;
+	}
+	/**
+	 * 设置：创建者
+	 */
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	/**
+	 * 获取：创建者
+	 */
+	public String getCreator() {
+		return creator;
+	}
+	/**
+	 * 设置：创建时间
+	 */
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	/**
+	 * 获取：创建时间
+	 */
+	public Date getCreateDate() {
+		return createDate;
+	}
+}
