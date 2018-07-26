42cec5d3f2e48fc04b5010d37e7cd208ac923b5e
king chen <396885563@qq.com>
2018-07-26 13:20:57 +0800

    单元测试、工具类

---
+++ oss/src/main/java/com/king/dao/OssDoc2pdfDao.java
@@ -0,0 +1,16 @@
+package com.king.dao;
+
+import com.king.dal.gen.model.oss.OssDoc2pdf;
+import com.king.dal.gen.dao.BaseDao;
+
+
+/**
+ * 文档转pdf、生成图片
+ * 
+ * @author king chen
+ * @email 396885563@qq.com
+ * @date 2018-07-25 10:14:40
+ */
+public interface OssDoc2pdfDao extends BaseDao<OssDoc2pdf> {
+	
+}
---
+++ oss/src/main/java/com/king/services/spi/OssDoc2pdfServiceImpl.java
@@ -0,0 +1,12 @@
+package com.king.services.spi;
+
+import org.springframework.stereotype.Service;
+import com.king.dal.gen.service.BaseServiceImpl;
+import com.king.dal.gen.model.oss.OssDoc2pdf;
+import com.king.api.oss.OssDoc2pdfService;
+
+@Service("ossDoc2pdfService")
+public class OssDoc2pdfServiceImpl extends BaseServiceImpl<OssDoc2pdf> implements OssDoc2pdfService {
+	
+	
+}
