ecae55c0cfd09c694ef08e110d34a67248cdc416
king chen <396885563@qq.com>
2018-07-31 18:14:47 +0800

    pdf添加水印

--- interface-service/src/main/java/com/king/api/oss/OssDoc2pdfService.java
+++ interface-service/src/main/java/com/king/api/oss/OssDoc2pdfService.java
@@ -1,6 +1,7 @@
 package com.king.api.oss;
 
 import com.king.dal.gen.model.oss.OssDoc2pdf;
+import com.king.dal.gen.model.oss.OssWaterSetting;
 import com.king.dal.gen.service.BaseService;
 
 
@@ -12,6 +13,13 @@ import com.king.dal.gen.service.BaseService;
  * @date 2018-07-25 10:14:40
  */
 public interface OssDoc2pdfService extends BaseService<OssDoc2pdf>{
-	
-	
+	/**
+	 * 根据ID，查询
+	 */
+	 OssWaterSetting queryWaterSetting(Object id);
+	/**
+	 * 更新
+	 */
+	void  saveOrUpdate(OssWaterSetting object);
+
 }
