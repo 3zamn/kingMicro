c53f3176a65244b8e32e975a5da69126b910e96e
king chen <396885563@qq.com>
2018-08-08 15:34:03 +0800

    优化日志

--- portal-html/src/main/webapp/static/js/modules/sys/log.js
+++ portal-html/src/main/webapp/static/js/modules/sys/log.js
@@ -10,8 +10,7 @@ $(function () {
 			{ label: '请求参数', name: 'params', width: 80 },
 			{ label: '返回结果', name: 'result', width: 100 },
 			{ label: '执行状态', name: 'status', width: 40 },
-          /*  { label: '执行时长(毫秒)', name: 'time', width: 80 },*/
-			{ label: 'IP地址', name: 'ip', width: 50 }, 			
+			{ label: '操作者IP', name: 'ip', width: 50 }, 			
 			{ label: '操作时间', name: 'createDate', width: 80 }			
         ],
 		viewrecords: true,
